370df9ccfd3680fb11b4e2afbf906cb7dc00d097
Sinkhé Soumaré <soumaresinkhe@gmail.com>
2026-01-21 10:13:51 +0000

    Initialisation du projet

---
+++ exoVersioLC.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+
+    int i, N, M, compteur, S;
+    float moyenne;
+
+    do{
+        printf("Veuillez entrer le nombre d'entiers : ");
+        scanf("%d",&N);
+
+        if(!N > 0)
+            puts("Erreur ! l'entier doit etre positif.");
+    }while(!(N > 0));
+    S = 0, compteur = 0;
+    for(i = 1; i <= N; i++){
+        do{
+            puts("Veuillez saisir un entier positif : ");
+            scanf("%d",&M);
+        }while(!(M > 0));
+
+        if(M % 2 == 0){
+            compteur++;
+            S += M;
+        }
+    }
+    moyenne = (float)S / compteur;
+    printf("La moyenne des entiers paires est : %.2f\n",moyenne);
+
+    return 0;
+}
